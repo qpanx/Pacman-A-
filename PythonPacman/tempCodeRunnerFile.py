625d895659b46993eda353fa34bded0d84040b82
Ahmed Wael Maarouf <98268557+qpanx@users.noreply.github.com>
2025-05-03 19:39:54 +0300

    Add files via upload

---
+++ PythonPacman/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def draw(self):
+        if (not powerup and not self.dead) or (eaten_ghost[self.id] and powerup and not self.dead):
+            screen.blit(self.img, (self.x_pos, self.y_pos))
+        elif powerup and not self.dead and not eaten_ghost[self.id]:
+            screen.blit(spooked_img, (self.x_pos, self.y_pos))
+        else:
+            screen.blit(dead_img, (self.x_pos, self.y_pos))
+        ghost_rect = pygame.rect.Rect((self.center_x - 18, self.center_y - 18), (36, 36))
+        return ghost_rect
